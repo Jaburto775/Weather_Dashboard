41e53da766b51d72900f0092285386eb14ccf4a4
Justin775 <jaburto775@gmail.com>
2021-06-08 23:56:41 -0500

    added function to create city listin js andadded 5 day forecast box

--- script.js
+++ script.js
@@ -14,11 +14,14 @@ function getDay(date){
     return showDate;
 }
 
+init();
+
 function init(){
     var storedCities = JSON.parse(localStorage.getItem("cities"));
         if (storedCities !== null) {
          cities = storedCities;
           }
+          renderCities();
 }
 
 function storeCities(){
@@ -26,4 +29,22 @@ function storeCities(){
    console.log(localStorage);
  }
 
- 
+ function renderCities() {
+    cityList.empty();
+        for (var i = 0; i < cities.length; i++) {
+        var city = cities[i];  
+        var li = $("<li>").text(city);
+        li.attr("id","listC");
+        li.attr("data-city", city);
+        li.attr("class", "list-group-item");
+        console.log(li);
+        cityList.prepend(li);
+        }
+   
+        if (!city){
+            return
+        } 
+        else{
+            //fail?
+        };
+}   
