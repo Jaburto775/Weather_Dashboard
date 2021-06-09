93ea90350805c6e5d96fb07f28b0686c186bca88
Justin775 <jaburto775@gmail.com>
2021-06-08 23:30:19 -0500

    added date format

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ var key = "509442dd782b4eae6dcca9e4317199c9";
 var cList =$("#city-list");
 var cities = [];
 
+function getDay(date){
+    var date = new Date();
+    console.log(date);
+    var month = date.getMonth()+1;
+    var day = date.getDate();
+    
+    var showDate = date.getFullYear() + '/' +
+        (month<10 ? '0' : '') + month + '/' +
+        (day<10 ? '0' : '') + day;
+    return showDate;
+}
 
